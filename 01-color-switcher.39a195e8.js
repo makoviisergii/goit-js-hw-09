a5f502040c8561b2aa488e8917662608a3c50241
makoviisergii <117633649+makoviisergii@users.noreply.github.com>
2023-02-03 20:26:15 +0000

    Deploying to gh-pages from @ makoviisergii/goit-js-hw-09@56355a0839081e76f175591186e1149508638fb0 🚀

--- 01-color-switcher.39a195e8.js
+++
@@ -1,2 +0,0 @@
-const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),d=document.body;t.addEventListener("click",(function(){a=setInterval((()=>{d.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3),t.disabled=!0,e.hasAttribute("disabled")&&(e.disabled=!1)})),e.addEventListener("click",(function(){clearInterval(a),e.disabled=!0,t.disabled=!1}));let a=null;
-//# sourceMappingURL=01-color-switcher.39a195e8.js.map
